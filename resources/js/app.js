ffaec40aca8423ac4c27b17b40374914eb9d92e7
Stanko Bebek <stanko.bebek82@gmail.com>
2021-02-20 16:25:06 +0100

    Article images

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,8 @@ import router from './router/index.js';
 import Vuetify from "../plugins/vuetify";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Vue from 'vue';
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/views/Article.vue
+++ resources/js/views/Article.vue
@@ -108,11 +108,34 @@
 
               </v-row>
 
-              <v-container v-html="notification.content" class="text-justify">
+              <v-container v-html="notification.content" class="mt-5 text-justify">
 
               </v-container>
+
           </v-container>
-          
+
+        <!--Article images-->
+        <h2 class="pt-10 mb-10"style="color: #1B4188;">Slike</h2>
+
+        <v-container fluid>
+            <v-row>
+                <v-col v-for="(image, index) in notification.images" :key="index" class="d-flex justify-center justify-md-start">
+                    <!-- img -->
+                    <a @click="toggleImage(image)">
+                      <v-img
+                      width="300"
+                      height="200"
+                      :src=" 'https://web-admin.sum.ba/api/storage/' + image"
+                    ></v-img>
+                    </a>
+
+                    <!-- img hidden -->
+                    <a @click="toggleImage(image)" class="lb" :id="image">
+                      <img :src=" 'https://web-admin.sum.ba/api/storage/' + image">
+                    </a>
+                </v-col>
+            </v-row>
+        </v-container>
             
         </v-container>
     </div>
@@ -120,9 +143,12 @@
 
 <script>
 import Navbar from '../components/Navbar.vue';
+import CoolLightBox from 'vue-cool-lightbox'
+import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
     export default {
         components:{
-            Navbar
+            Navbar,
+            CoolLightBox
         },
         data(){
             return{
@@ -152,6 +178,11 @@ import Navbar from '../components/Navbar.vue';
             goToArticle(alias){
                 this.$router.push({name: 'Article', params: {alias: alias}})
                 location.reload()
+            },
+            toggleImage(image){
+                let element = document.getElementById(image)
+
+                element.classList.toggle('lb_target')
             }
         },
         computed: {
@@ -161,8 +192,6 @@ import Navbar from '../components/Navbar.vue';
                 let notifications = this.notifications.filter(notification => {
                     return notification.id !== this.notification_id
                 })
-
-                console.log("hahaha", notifications)
 
                 if(not_len > 2){
                     return [notifications[0], notifications[1]]   
@@ -242,10 +271,38 @@ import Navbar from '../components/Navbar.vue';
     }
 
     #social-btn-2{
-        background: rgb(64,93,230) !important;
+        background: rgb(251, 173, 80) !important;
     }
 
     #social-btn-3{
         background: rgb(255, 0, 0) !important;
     }
+
+    /*Article images CSS*/
+    .lb {
+    display: none;
+    position: fixed!important;
+    z-index: 999;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0,0,0,0.8);
+    text-align:center;
+    }
+
+    .lb img {
+        max-width: 90%;
+        max-height: 80%;
+        margin-top: 2%;
+    }
+
+    .lb_target {
+        outline: none;
+        display: block;
+    }
+
+    a.lb:hover {cursor:default;}
+    img:hover {cursor:pointer;}
+
 </style>
